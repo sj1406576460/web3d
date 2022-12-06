2a342084e38058e47413ab88a37c2e3ed2501c83
frank <Fsong@ashleyfurniture.com>
2022-12-06 10:21:54 +0800

    'update'

--- three/app2.js
+++ three/app2.js
@@ -3,7 +3,7 @@ let scene, camera, group, renderer, composer, outlinePass;
 const init = () => {
 	// Scene
 	scene = new THREE.Scene();
-	scene.background = new THREE.Color(0x000000, 0.01);
+	//scene.background = new THREE.Color(0x000000, 0.01);
 
 	// Renderer
 	renderer = new THREE.WebGLRenderer({
@@ -12,7 +12,7 @@ const init = () => {
 	});
 	renderer.shadowMap.enabled = true // 显示阴影
 	renderer.shadowMap.type = THREE.PCFSoftShadowMap
-	renderer.setClearColor(0x000000, 1) // 设置背景颜色
+	//renderer.setClearColor(0xffffff, 1) // 设置背景颜色
 	renderer.setPixelRatio(window.devicePixelRatio);
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	document.body.appendChild(renderer.domElement);
@@ -241,8 +241,8 @@ const init = () => {
 		outlinePass.renderToScreen = true;
 		//outlinePass.pulsePeriod = 1 //闪烁
 		outlinePass.usePatternTexture = false //是否使用贴图
-		outlinePass.visibleEdgeColor.set(0xffffff); // 高光颜色0xff0000
-		outlinePass.hiddenEdgeColor.set(0xffffff); // 阴影颜色
+		outlinePass.visibleEdgeColor.set('#EB3C00'); // 高光颜色0xff0000
+		outlinePass.hiddenEdgeColor.set('#EB3C00'); // 阴影颜色
 		outlinePass.usePatternTexture = false; //是否使用父级的材质
 		outlinePass.downSampleRatio = 2; // 边框弯曲度
 
@@ -250,7 +250,7 @@ const init = () => {
 		effectFXAA.uniforms['resolution'].value.set(1 / window.innerWidth, 1 / window.innerHeight);
 		composer.addPass(effectFXAA);
 
-		//outlinePass.clear = true;
+		outlinePass.clear = true;
 		composer.addPass(outlinePass)
 	}
 
